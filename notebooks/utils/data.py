8a92203b05bd1140bc6701a10b161043faeef80b
daviseemann <daviseemann@gmail.com>
2026-04-17 17:35:55 -0300

    Add data generation and plotting utilities for Gaussian distributions
    - Implemented `generate_data` function to create synthetic Gaussian data with specified means, covariances, and priors.
    - Added `plot_2D` function for visualizing 2D Gaussian data and its class distributions.
    - Introduced `plot_features` function to plot histograms of features for each class.
    - Removed the old utility functions from `utils.py` as they are no longer needed.

---
+++ notebooks/utils/data.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import multivariate_normal
+
+
+def generate_data(
+    n_samples, means, covs, priors=None, seed=42, groups=None, **kwargs
+) -> np.ndarray:
+    """Gera um DataFrame com n-classes de dados gaussianos."""
+
+    n_classes = len(means)
+
+    if priors is None:
+        priors = [1 / n_classes] * n_classes
+
+    assert round(sum(priors), 2) == 1, "Os priors devem somar 1."
+    assert (
+        len(priors) == n_classes
+    ), "O número de priors deve ser igual ao número de classes."
+
+    class_samples = []
+    for i in range(n_classes):
+        class_pdf = multivariate_normal(means[i], covs[i])
+        samples = class_pdf.rvs(int(n_samples * priors[i]), random_state=seed + 5 + i)
+        class_samples.append(samples)
+
+    data = np.vstack(class_samples)
+    labels = np.concatenate(
+        [np.array([i] * len(samples)) for i, samples in enumerate(class_samples)]
+    )
+
+    if groups is None:
+        groups = labels  # Cada classe é um grupo diferente
+
+    return data, labels, groups
---
+++ notebooks/utils/plot.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_2D(data: np.array, labels: np.array) -> None:
+
+    n_classes = np.unique(labels).size
+
+    fig = plt.figure(figsize=(12, 10))
+    gs = fig.add_gridspec(2, 2, height_ratios=[2, 1])
+
+    ax1 = fig.add_subplot(gs[0, :])  # ocupa as 2 colunas (gráfico 1)
+    ax2 = fig.add_subplot(gs[1, 0])  # gráfico 2
+    ax3 = fig.add_subplot(gs[1, 1])  # gráfico 3
+
+    ax1.scatter(data[:, 0], data[:, 1], c=labels, cmap="viridis", edgecolor="k")
+    ax1.set_xlabel("x1")
+    ax1.set_ylabel("x2")
+    ax1.set_title("Dados Gaussianos")
+    ax1.grid()
+
+    for i in range(n_classes):
+        class_data = data[labels == i]
+        ax2.hist(class_data[:, 0], alpha=0.5, label=f"Classe {i}")
+    ax2.set_xlabel("x1")
+    ax2.set_ylabel("Frequência")
+    ax2.set_title("Distribuição de x1 por Classe")
+    ax2.legend()
+
+    for i in range(n_classes):
+        class_data = data[labels == i]
+        ax3.hist(class_data[:, 1], alpha=0.5, label=f"Classe {i}")
+    ax3.set_xlabel("x2")
+    ax3.set_ylabel("Frequência")
+    ax3.set_title("Distribuição de x2 por Classe")
+    ax3.legend()
+
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_features(data, labels):
+
+    n_classes = np.unique(labels).size
+    n_features = data.shape[1]
+
+    fig, axes = plt.subplots(
+        n_classes, n_features, figsize=(3 * n_features, 2 * n_classes)
+    )
+    for i in range(n_classes):
+        for j in range(n_features):
+            axes[i, j].hist(data[labels == i, j], bins=20, alpha=0.7)
+            axes[i, j].set_title(f"Classe {i} - Feature {j}")
+    plt.tight_layout()
+    plt.show()
